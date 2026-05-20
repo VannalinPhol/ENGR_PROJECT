2056396e35d7de801486193ce5695c37c732f046
VannalinPhol <pholvannalin@gmail.com>
2026-05-20 21:05:26 +1000

    Update dcmotor.py

--- dcmotor.py
+++ dcmotor.py
@@ -4,30 +4,26 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
-# Pins from your reference image
 PWMA = 12
-AIN2 = 18
 AIN1 = 16
+AIN2 = 18
 STBY = 22
 
-PWM_FREQ = 100
-
 GPIO.setup(PWMA, GPIO.OUT)
-GPIO.setup(AIN2, GPIO.OUT)
 GPIO.setup(AIN1, GPIO.OUT)
+GPIO.setup(AIN2, GPIO.OUT)
 GPIO.setup(STBY, GPIO.OUT)
 
-pwm = GPIO.PWM(PWMA, PWM_FREQ)
+pwm = GPIO.PWM(PWMA, 100)
 pwm.start(0)
 
 try:
-    print("Motor A forward")
     GPIO.output(STBY, GPIO.HIGH)
 
+    print("Two motors forward together")
     GPIO.output(AIN1, GPIO.HIGH)
     GPIO.output(AIN2, GPIO.LOW)
-
-    pwm.ChangeDutyCycle(70)
+    pwm.ChangeDutyCycle(80)
     sleep(5)
 
     print("Stop")
@@ -35,9 +31,6 @@ try:
     GPIO.output(AIN1, GPIO.LOW)
     GPIO.output(AIN2, GPIO.LOW)
     GPIO.output(STBY, GPIO.LOW)
-
-except KeyboardInterrupt:
-    print("Stopped")
 
 finally:
     pwm.stop()
